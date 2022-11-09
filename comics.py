f2fde2c8f4edfb871d91481cf9a48859b8137378
Tim Mackenzie <theymightbetim@gmail.com>
2022-11-09 16:54:37 -0500

    remove extra whitespace

--- comics.py
+++ comics.py
@@ -1,12 +1,9 @@
 import requests
 from datetime import date
-import json
-
 def get_todays_new_comics():
     today = date.today()
     format=today.strftime("%m/%d/%Y")
     print(format)
-
     url = f'https://www.previewsworld.com/NewReleases/Export?format=txt&releaseDate={format}'
     response = requests.get(url)
     if response.text != '':
