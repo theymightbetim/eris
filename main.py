f1a77f163927006caedb72795651f17f11f39ce2
Tim Mackenzie <theymightbetim@gmail.com>
2025-02-19 01:27:18 -0500

    task: cleanup debug code

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from bot.eris import Eris
 from discord import Intents
-import logging
-import bot.settings
 
 if __name__ == "__main__":
-    print(bot.settings.LOG_PATH)
-    print(bot.settings.ROOT_DIR)
     golden_intentions = Intents.default()
     golden_intentions.message_content = True
     golden_intentions.members = True
